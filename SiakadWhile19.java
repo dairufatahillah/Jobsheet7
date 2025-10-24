a3f69d674f61545539ba0bb5f5f653dd6df4180f
Dairu Fatahillah <dairufatahillah60@gmail.com>
2025-10-24 18:04:44 +0700

    SiakadWhile06

---
+++ SiakadWhile19.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class SiakadWhile19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nilai = 0, jml = 0, i = 0;
+
+        System.out.print("Masukan jumlah mahasiswa: ");
+        jml = sc.nextInt();
+
+        while (i < jml) {
+            System.out.print("Masukan nilai mahasiswa ke-" + (i + 1) + " : ");
+            nilai = sc.nextInt();
+
+            if (nilai < 0 || nilai > 100) {
+                System.out.println("Nilai tidak valid, masukan antara 0-100");
+                continue;
+            }
+
+            if (nilai >= 80 && nilai <= 100) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " : A");
+
+            } else if (nilai >= 73 && nilai < 80) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " : B+");
+
+            } else if (nilai >= 65 && nilai < 73) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " : B");
+
+            } else if (nilai >= 60 && nilai < 65) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " : C+");
+
+            } else if (nilai >= 50 && nilai < 60) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " : C");
+
+            } else if (nilai >= 39 && nilai < 50) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " : D");
+                
+            } else {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " : E");
+            }
+
+            i++;
+        }
+    }
+}
